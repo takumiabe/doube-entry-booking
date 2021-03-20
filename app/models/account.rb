a05a654d7718f1c198a45a41fc387b74e1bb71a8
Takumi Abe <abe@engraphia.com>
2021-03-20 19:52:29 +0900

    add Account#current_statement

--- app/models/account.rb
+++ app/models/account.rb
@@ -4,4 +4,22 @@ class Account < ApplicationRecord
 
   enum xacttype: { credit: 'cr', debit: 'dr' }
   validates :name, :xacttype, presence: true
+
+  def current_statement
+    last_statement = account_statements.where(date: Date.today.beginning_of_month).first
+
+    xaction_this_month = account_transactions.where(created_at: Date.today.all_month)
+    total_credit = xaction_this_month.where(xacttype_ext: [:adjust_credit, :deposit]).sum(:amount)
+    total_debit = xaction_this_month.where.not(xacttype_ext: [:adjust_credit, :deposit]).sum(:amount)
+
+    current_balance = (last_statement&.closing_balance || 0) + total_credit - total_debit
+
+    {
+      account: self.name,
+      date: Date.today.beginning_of_month,
+      total_credit: total_credit,
+      total_debit: total_debit,
+      current_balance: current_balance
+    }
+  end
 end
