7c2182723231709b1c8a95d3bb7cdc108423feb7
Takumi Abe <abe@engraphia.com>
2021-03-20 17:44:41 +0900

    add factory_bot_rails

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,8 @@ module DoubleEntryBooking
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.generators do |g|
+      g.fixtures = :factory_bot
+    end
   end
 end
